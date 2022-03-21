2172255cebb9aff628f0032ed1a4d0262ff9ef71
Kevin Do <kevind0501@gmail.com>
2022-03-21 12:12:52 -0700

    passing third exercise

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -2,3 +2,17 @@
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
 
+SELECT 
+  title
+FROM 
+  film
+LEFT JOIN
+  film_category
+ON 
+  film.film_id = film_category.film_id
+LEFT JOIN
+  category
+ON 
+  category.category_id = film_category.category_id
+WHERE
+  category.name = 'Action'
