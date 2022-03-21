e1eecb09d47fd3b83788b456c01f3513d203c1b5
Kevin Do <kevind0501@gmail.com>
2022-03-21 12:54:52 -0700

    passing 8th exercise

--- exercise_8/exercise-8.sql
+++ exercise_8/exercise-8.sql
@@ -4,7 +4,7 @@
 
 SELECT
   title
-FROM 
+FROM
   film
 LEFT JOIN
   inventory
@@ -14,5 +14,22 @@ LEFT JOIN
   rental
 ON
   inventory.inventory_id = rental.inventory_id
-
+LEFT JOIN
+  customer
+ON 
+  rental.customer_id = customer.customer_id
+LEFT JOIN
+  address
+ON
+  customer.address_id = address.address_id
+LEFT JOIN
+  city
+ON
+  address.city_id = city.city_id
+LEFT JOIN
+  country
+ON
+  city.country_id = country.country_id
+WHERE
+  country.country = 'Peru'
 
