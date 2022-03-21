b15a492b7c2a9e43f26c02aa61d45d545b930127
Kevin Do <kevind0501@gmail.com>
2022-03-21 12:46:44 -0700

    passing exercise 7

--- exercise_8/exercise-8.sql
+++ exercise_8/exercise-8.sql
@@ -1,2 +1,18 @@
 -- find all film titles rented out by customers who live in the country of Peru
 -- Similar to exercise 7 with an additional join to get country name
+-- film -> inventory -> rental -> customer -> address -> city -> country
+
+SELECT
+  title
+FROM 
+  film
+LEFT JOIN
+  inventory
+ON
+  film.film_id = inventory.film_id
+LEFT JOIN
+  rental
+ON
+  inventory.inventory_id = rental.inventory_id
+
+
