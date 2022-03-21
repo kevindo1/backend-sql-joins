71ecda858015b905580deab6d6f7d7f591c14272
Kevin Do <kevind0501@gmail.com>
2022-03-21 12:04:57 -0700

    passing 2nd test

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,2 +1,13 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
+
+SELECT
+  *
+FROM
+  city
+INNER JOIN
+  address
+ON 
+  city.city_id = address.city_id 
+WHERE
+  address.address_id=146
--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,4 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+
